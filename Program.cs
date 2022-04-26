25893c7251a3393c4f361814bdf789a421df260e
ria10 <riaranderwala@gmail.com>
2022-04-26 13:42:48 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace PushPop_ToDo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
